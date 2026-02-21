0e72946276b05f871ed9ab438d397b8ee595dfee
Rujuta-133 <rantrolikar@gmail.com>
2026-02-21 16:12:55 +0530

    feat: add CLI command handling and task serialization methods

--- src/orbit/cli.py
+++ src/orbit/cli.py
@@ -1,14 +1,36 @@
 from orbit.task_manager import TaskManager
+import sys
 
 
 def main():
-    print("Orbit running")
+    if len(sys.argv) < 2:
+        print("Please write a command as 'orbit <command>'")
+        print("Example: 'orbit list'")
+        sys.exit()
+
+    command = sys.argv[1]
     manager = TaskManager()
-    first_task = manager.add_task("First Task")
-    second_task = manager.add_task("Second Task")
-    tasks = manager.list_current_tasks()
-    for task in tasks:
-        print(task)
+    if command == "list":
+        
+        tasks = manager.list_current_tasks()
+        if len(tasks) == 0:
+            print("No tasks found")
+        else:
+            for task in tasks:
+                print(task)
+    elif command == "add":
+        if len(sys.argv) < 3:
+            print("Description Required")
+            sys.exit()
+            
+        description = sys.argv[2]
+        task = manager.add_task(description)
+        print(f"Added task: {task.id} {task.description}")
+        
+    else:
+        print("Unknown command")
+
+        
 
 if __name__ == "__main__":
     main()
